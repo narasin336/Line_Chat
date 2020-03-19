95105a541d58112ad50aa15ed63c0f17ab0e85bf
narasin336 <61955117+narasin336@users.noreply.github.com>
2020-03-19 13:23:19 +0700

    Add a new Project

---
+++ Line_Chat/Project/Config.py
@@ -0,0 +1,3 @@
+Channel_secret = 'e58a0f7fb481e1fcee39a064755117e6'
+Channel_access_token = '22C80b9BzNZY3NLQBWaJ6W0CCBr5sCl/9GZkjf7OFU7FGpYvL7qitaJq+31o3pBPBulLNfEr28MO0Jo8ZnTbfS+DlbmK9/+ufDDSnqflGb/CHG+OrywvxUrmNECRCmKY9CezTTDvnzeP+DZhz+zi/gdB04t89/1O/w1cDnyilFU='
+basic_id = '@147lvcjv'
